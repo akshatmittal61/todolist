dd9241a728096a33c8ab96a0c8c1b0a50d204ead
akshatmittal61 <akshatmittal2506@gmail.com>
2021-12-16 22:17:51 +0530

    Added mongo DB database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import _ from "lodash";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
@@ -10,39 +11,89 @@ const __fileName = fileURLToPath(import.meta.url);
 const __dirName = dirname(__fileName);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todoListDB");
 
-/* let items = [];
-let workItems = []; */
+let itemsToSend = [];
+let allRoutes = [];
+let routes = [];
+
+const itemsSchema = new mongoose.Schema({
+	title: String,
+	category: String,
+});
+const Item = new mongoose.model("Item", itemsSchema);
+Item.find({}, (err, data) => {
+	if (err) console.log(err);
+	else {
+		data.forEach((item) => {
+			allRoutes = [...allRoutes, item.category];
+		});
+		routes = [...new Set(allRoutes)];
+		console.log(routes);
+	}
+});
+let today = new Date();
+let options = {
+	weekday: "long",
+	day: "numeric",
+	month: "long",
+};
+let day = today.toLocaleDateString("en-US", options);
+
 app.get("/", (req, res) => {
-	let today = new Date();
-	let options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-	};
-	let day = today.toLocaleDateString("en-US", options);
-	res.render("list", { title: day, items: items });
+	itemsToSend = [];
+	Item.find({}, (err, data) => {
+		if (err) console.log(err);
+		else {
+			data.forEach((item) => {
+				if (item.category === `/`) itemsToSend = [...itemsToSend, item];
+			});
+			console.log(data);
+			console.log(itemsToSend);
+			res.render("list", {
+				title: day,
+				items: itemsToSend,
+				route: "/",
+				allRoutes: routes,
+			});
+		}
+	});
 });
 app.post("/", (req, res) => {
 	console.log(req.body);
-	if (req.body.button === "Work") {
-		workItems = [...workItems, req.body.item];
-		res.redirect("/work");
-	} else {
-		items = [...items, req.body.item];
-		res.redirect("/");
-	}
-});
-app.get("/work", (req, res) => {
-	res.render("list", { title: "Work", items: workItems });
-});
-app.post("/work", (req, res) => {
-	console.log(req.body);
-	workItems = [...items, req.body.item];
-	res.redirect("/work");
+	const itemTitle = req.body.item;
+	const newItem = new Item({
+		title: itemTitle,
+		category: "/",
+	});
+	newItem.save();
+	itemsToSend = [...itemsToSend, req.body.item];
+	res.redirect("/");
 });
 app.get("/about", (req, res) => {
 	res.render("about");
+});
+
+app.post("/delete", (req, res) => {
+	const checkId = req.body.checked;
+	let itemToDel = {};
+	Item.findById(checkId, (err, item) => {
+		if (err) console.log(err);
+		else {
+			console.log(item);
+			itemToDel = { ...item._doc };
+		}
+	});
+	Item.findByIdAndDelete(checkId, (err) => {
+		if (err) console.log(err);
+		else
+			console.log({
+				status: 200,
+				message: `Suuccessfully deleted item with id: ${checkId}`,
+			});
+		console.log(itemToDel);
+		res.redirect(itemToDel.category);
+	});
 });
 
 app.listen(5000, () => {
